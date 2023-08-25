f7fb38522488a83f2f913792779a1a5b5bd915f6
infinity-ocean <varrr7@gmail.com>
2023-08-25 18:38:50 +0200

    + First working feed

--- Conexiune/dialogs/standart_user/FeedMenu/getters.py
+++ Conexiune/dialogs/standart_user/FeedMenu/getters.py
@@ -1,36 +1,38 @@
 from aiogram.enums import ContentType
 from aiogram_dialog.api.entities import MediaAttachment, MediaId
 
-from dialogs.standart_user.FeedMenu.getter_api import get_profiles
+from dialogs.standart_user.FeedMenu.getter_api import less_4h
+from dialogs.standart_user.FeedMenu.w_api import slct20_handle_back, slct_p_ram
 
 
 async def getter_w1(**kwargs):
-    manager = kwargs['dialog_manager']
+    dialog_data = kwargs['dialog_manager'].dialog_data
 
-    # Time-check
-    # Over-check
-    # if await less_4h(redis['time_mark']): 1
-    # if redis['pointer'] == 20:
-    # before the select -- await get_preferences() --
-    if True:  # 1
-        if True:
-            p_list: list = await get_profiles(kwargs['session'])
-            counter = 0
-            manager.dialog_data['profiles'] = []
-            for p in p_list:
-                manager.dialog_data['profiles'].append({
-                    'photo': p.photo, 'name': p.name,
-                    'age': p.age, 'sex': p.sex,
-                    'location': p.location, 'description': p.descr
-                })
-            p = p_list[0]
-            return {
-                'photo': MediaAttachment(
-                    ContentType.PHOTO, file_id=MediaId(p.photo)
-                ),
-                'name': p.name,
-                'age': p.age,
-                'sex': p.sex,
-                'location': p.location,
-                'descr': p.descr,
-            }
+    # before the select -- await get_preferences() -> preferences
+    if 'time_mark' not in dialog_data:
+        p = await slct20_handle_back(kwargs['session'], dialog_data, offset=False)
+    else:
+        if await less_4h(dialog_data['time_mark']):
+            if dialog_data['pointer'] == 19:
+                p = await slct20_handle_back(kwargs['session'], dialog_data, offset=True)
+            else:  # MAIN FLOW
+                p_dict = await slct_p_ram(dialog_data)
+                return {
+                    'photo': MediaAttachment(ContentType.PHOTO, file_id=MediaId(p_dict['photo'])),
+                    'name': p_dict['name'],
+                    'age': p_dict['age'],
+                    'sex': p_dict['sex'],
+                    'location': p_dict['location'],
+                    'descr': p_dict['descr'],
+                }
+
+        else:
+            p = await slct20_handle_back(kwargs['session'], dialog_data, offset=False)
+    return {
+        'photo': MediaAttachment(ContentType.PHOTO, file_id=MediaId(p.photo)),
+        'name': p.name,
+        'age': p.age,
+        'sex': p.sex,
+        'location': p.location,
+        'descr': p.descr,
+    }
